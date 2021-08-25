34e45ab02a108a850ded9c590e63705c13b0e027
Jack Hegarty <jmhegarty14@gmail.com>
2021-08-24 20:38:42 -0700

    Updates

---
+++ precache-manifest.9d36601970e5fa19841a4af6055d5442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d514a79ba5301015554",
+    "url": "/home/static/css/main.f0902b63.chunk.css"
+  },
+  {
+    "revision": "1d514a79ba5301015554",
+    "url": "/home/static/js/main.1d514a79.chunk.js"
+  },
+  {
+    "revision": "5fdf7f86495bc64854ab",
+    "url": "/home/static/js/runtime~main.5fdf7f86.js"
+  },
+  {
+    "revision": "4bbc24d561c602cc6e5c",
+    "url": "/home/static/js/2.4bbc24d5.chunk.js"
+  },
+  {
+    "revision": "ed7478decb9343baaf3b38fe6439fee5",
+    "url": "/home/static/media/hero_background.ed7478de.png"
+  },
+  {
+    "revision": "27e8cf177f825d49958842a2d78d565a",
+    "url": "/home/index.html"
+  }
+];
